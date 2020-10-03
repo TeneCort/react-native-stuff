7c3084bbfc60a3dacb2e8d9985017e829bda3392
Ioannis <ioannis.karapostolis@uha.fr>
2020-10-03 14:41:24 +0200

    blank proj

--- App.js
+++ App.js
@@ -1,69 +1,16 @@
-import React, {useState} from 'react';
-import { FlatList, StyleSheet, View, Alert, TouchableWithoutFeedback, Keyboard} from 'react-native';
-import Header from './components/header';
-import TodoItem from './components/todoItem';
-import AddTodo from './components/addTodo';
+import React from 'react';
+import {View} from 'react-native';
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    {text: 'buy coffee', key: '1'},
-    {text: 'create an app', key: '2'},
-    {text: 'play on the switch', key: '3'},
-  ]);
-  // How does setTodos know what prevTodos is?
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.key != key);
-    })
-  }
-
-  const submitHandler = (text) => {
-    if(text.length > 3)
-    {
-      setTodos((prevTodos) => {
-        return [
-          {text: text, key: Math.random().toString()},
-          ...prevTodos
-        ]
-      });
-    }else{
-      Alert.alert('Oops', 'Todos must be over 3 chars long!', [
-        {text: 'Understood', onPress: () => console.log('alert closed')}
-      ]);
-    }
-  }
-
+  
   return (
-    <TouchableWithoutFeedback onPress={() =>{
-      Keyboard.dismiss();
-    }}>
-      <View style={styles.container}>
-        <Header/>
-        <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler}/>
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({item}) => (
-                <TodoItem item={item} pressHandler={pressHandler}></TodoItem>
-              )}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+  <View style={styles.list}>
+
+  </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  content: {
-    flex: 1,
-    padding: 40,
-  },
   list: {
     flex: 1,
     marginTop: 20,
